1ed43876967e1b447cc29cb448a492c6ee6b7b15
YHaoNan <1355265122@qq.com>
2019-06-21 09:26:53 +0800

    Site updated: 2019-06-21 09:26:53

--- main.54734ab978f98119b312.zh-Hans.js
+++ main.54734ab978f98119b312.zh-Hans.js
@@ -1,6 +1,6 @@
 var articleObj = document.getElementsByTagName('article');
 console.log(articleObj);
-if (articleObj && articleObj.length>0){
+if (articleObj!=null && articleObj.length>0){
     console.log('hasArticle');
     var adDiv = document.createElement('div');
     adDiv.innerHTML = '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><!-- adpage --><ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-8637197148903783" data-ad-slot="2008916863" data-ad-format="auto" data-full-width-responsive="true"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>';
